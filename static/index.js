de71653f48a6a532339eae28b7c95c86838988f0
oxy-Op <mdrehaan7766@gmail.com>
2023-09-27 13:17:29 +0530

    completed matrix gen ui

--- static/index.js
+++ static/index.js
@@ -57,43 +57,9 @@ const setTheme = theme => {
     }
 }
 
-// ---------------------------------------------------- //
+// ---------------------- Matrix Multiplication------------------------- //
 
-// const thrice_matrix = ```<div class="matrix" id="matrix1">
-//     <div class="row w-100">
-//         <div class="col-4">
-//             <input class="w-100" id="m1-1-1" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//         <div class="col-4">
-//             <input class="w-100" id="m1-1-2" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//         <div class="col-4">
-//             <input class="w-100" id="m1-1-3" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//     </div>
-//     <div class="row w-100">
-//         <div class="col-4">
-//             <input class="w-100" id="m1-2-1" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//         <div class="col-4">
-//             <input class="w-100" id="m1-2-2" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//         <div class="col-4">
-//             <input class="w-100" id="m1-2-3" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//     </div>
-//     <div class="row w-100">
-//         <div class="col-4">
-//             <input class="w-100" id="m1-3-1" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//         <div class="col-4">
-//             <input class="w-100" id="m1-3-2" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//         <div class="col-4">
-//             <input class="w-100" id="m1-3-3" type="number" min="-100" max="100" value="0" required="">
-//         </div>
-//     </div>
-// </div>```
+
 
 // const matrix1 = $('.matrix#matrix1');
 // const row1 = matrix1.find('.row.w-100').eq(0);
@@ -120,34 +86,83 @@ $(document).ready(() => {
     hideSidbar();
     showSidebar();
     mediaQuery();
+    localStorage.getItem('matrix') ? addCards(parseInt(localStorage.getItem('matrix'))) : addCards(2);
     switchDimension();
+    addMatrix3()
+    removeMatrix3()
+    checkForMatrix3();
+    activeDimension();
+
 })
 
+function addCards(num, dimension = localStorage.getItem('dimension') == 'twice' ? 2 : 3) {
+    $('#cards-container').empty();
+    for (let i = 1; i <= num; i++) {
+        $('#cards-container').append(generateCard(i, dimension))
+    }
+}
 
+function addMatrix3() {
+    $('#add-matrix').on('click', function () {
+        localStorage.setItem('matrix', '3')
 
+        addCards(3)
+        checkForMatrix3()
+    })
+}
 
+function removeMatrix3() {
+    $('#remove-matrix').on('click', function () {
+        addCards(2)
+        localStorage.setItem('matrix', '2')
+        checkForMatrix3()
+    })
+}
+
+function checkForMatrix3() {
+    if (localStorage.getItem('matrix') === '3') {
+        $('#add-matrix').hide()
+        $('#remove-matrix').show()
+    }
+    else {
+        $('#add-matrix').show()
+        $('#remove-matrix').hide()
+    }
+}
+
+function generateCard(id, dimension) {
+    const card = $('<div>', { class: 'card mb-md-0 mb-4', style: 'width: 200px;' });
+    const cardHeader = $('<div>', { class: 'card-header d-flex justify-content-around' }).appendTo(card);
+    const cardBody = $('<div>', { class: 'card-body w-100' }).appendTo(card);
+    const cardFooter = $('<div>', { class: 'card-footer' }).appendTo(card);
+    const h5 = $('<h5>', { class: 'text-center', text: `Matrix ${id}` }).appendTo(cardFooter);
+    cardBody.append(new Matrix(id, dimension).createMatrix());
+    return card
+}
 class Matrix {
-    constructor(id) {
+    constructor(id, dimension) {
         this.id = id;
+        this.dimension = dimension
     }
 
     createMatrix() {
         let matrix = $('<div>', { class: 'matrix', id: 'matrix' + this.id.toString() });
-        for (let i = 1; i <= 3; i++) {
+        for (let i = 1; i <= this.dimension; i++) {
             matrix.append(this.createRow(i));
         }
         return matrix;
     }
     createRow(nrow) {
         const row = $('<div>', { class: 'row w-100' });
-        for (let i = 1; i <= 3; i++) {
+        for (let i = 1; i <= this.dimension; i++) {
             row.append(this.createCol(nrow, i));
         }
         return row;
     }
 
     createCol(row, ncol) {
-        const col = $('<div>', { class: 'col-4' });
+        const className = this.dimension === 3 ? 'col-4' : 'col-6';
+        const col = $('<div>', { class: className });
         col.append(this.createInput(row, ncol));
         return col;
     }
@@ -158,54 +173,54 @@ class Matrix {
     }
 }
 
+function generateSign() {
+    const div = $('<div>', { class: 'sign' });
+}
 
-function changeDimension(element, dimension) {
-    const card = element.parents('.card')
-    const cardBody = card.find('.card-body')
-    const matrix = cardBody.find('.matrix')
 
+function changeDimension(dimension) {
+    const id = (localStorage.getItem('matrix')) ? parseInt(localStorage.getItem('matrix')) : 2
     if (dimension === 'twice') {
-        const lastRow = matrix.children().last()
-        const firstTwoRows = matrix.children().not(lastRow);
-        for (let row of firstTwoRows) {
-            const lastCol = $(row).children().last()
-            const firstTwoCol = $(row).children().not(lastCol)
-            for (let col of firstTwoCol) {
-                $(col).removeClass('col-4')
-                $(col).addClass('col-6')
-            }
-            lastCol.remove()
-        }
-        lastRow.remove()
+        addCards(id, 2)
+        localStorage.setItem('dimension', 'twice')
     }
     if (dimension === 'thrice') {
-        cardBody.find('.matrix').html(thrice_matrix)
-        console.log(element);
+        addCards(id, 3)
+        localStorage.setItem('dimension', 'thrice')
+    }
+}
+
+function activeDimension() {
+    const activeBtn = localStorage.getItem('dimension')
+    if (activeBtn === 'twice') {
+        $('.twice').addClass('active')
+        $('.thrice').removeClass('active')
+    }
+    if (activeBtn === 'thrice') {
+        $('.thrice').addClass('active')
+        $('.twice').removeClass('active')
     }
 }
 
 function switchDimension() {
+
     const twiceBtn = $('.twice')
     const thriceBtn = $('.thrice')
     $(twiceBtn).on('click', function () {
-        changeDimension(twiceBtn, 'twice')
+        changeDimension('twice')
         twiceBtn.addClass('active')
         thriceBtn.removeClass('active')
     })
 
     $(thriceBtn).on('click', function () {
-        changeDimension(thriceBtn, 'thrice')
+        changeDimension('thrice')
         thriceBtn.addClass('active')
         twiceBtn.removeClass('active')
     })
 }
 
 
-
-
-
-
-
+// ------------Responsiveness and Color modes --------------- //
 
 window.onresize = (function () {
     mediaQuery()
