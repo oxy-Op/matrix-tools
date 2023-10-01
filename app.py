b23d6ac7a8657099cb1c14db28ef5c67cd521232
oxy-Op <mdrehaan7766@gmail.com>
2023-10-01 22:36:58 +0530

    finished

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, jsonify, render_template, request
 from os import path, walk
 
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
+app = Flask(__name__)
 
-extra_dirs = ['templates','static']
-extra_files = extra_dirs[:]
-for extra_dir in extra_dirs:
-    for dirname, dirs, files in walk(extra_dir):
-        for filename in files:
-            filename = path.join(dirname, filename)
-            if path.isfile(filename):
-                extra_files.append(filename)
+# extra_dirs = ['templates','static']
+# extra_files = extra_dirs[:]
+# for extra_dir in extra_dirs:
+#     for dirname, dirs, files in walk(extra_dir):
+#         for filename in files:
+#             filename = path.join(dirname, filename)
+#             if path.isfile(filename):
+#                 extra_files.append(filename)
 
 
 def matrix_calculation(data):
@@ -40,7 +40,7 @@ def matrix():
     result = matrix_calculation(request.json)
     return jsonify(result)
 
-if __name__ == "__main__":
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATE_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0',debug=True, port=5200 , extra_files=extra_files)
+# if __name__ == "__main__":
+#     app.jinja_env.auto_reload = True
+#     app.config['TEMPLATE_AUTO_RELOAD'] = True
+#     app.run(host='0.0.0.0',debug=True, port=5200 , extra_files=extra_files)
